e8da0233255d4b0766cdbf6c8175df2734804b8f
Yadu Krishna <yaduk946@gmail.com>
2024-01-21 09:26:20 +0530

    added tilt animation to group cards

--- src/Components/GroupCard.tsx
+++ src/Components/GroupCard.tsx
@@ -17,7 +17,7 @@ const defaultOptions = {
   reverse: true, // reverse the tilt direction
   max: 35, // max tilt rotation (degrees)
   perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-  scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+  scale: 1.05, // 2 = 200%, 1.5 = 150%, etc..
   speed: 1000, // Speed of the enter/exit transition
   transition: true, // Set a transition on enter/exit.
   axis: null, // What axis should be disabled. Can be X or Y.
@@ -44,10 +44,12 @@ export default function GroupCard(props: cardType) {
                   boxShadow: "0 2px 5px rgba(150, 150, 150, 0.35)",
                   borderRadius: "10px",
                   border: "3px  rgba(44, 41, 41, 0.400",
-                  backgroundColor: "rgb(30,60,30)",
+                  backgroundImage:
+                    "linear-gradient(to top,rgb(17, 29, 60), rgb(100, 35, 35))",
                   color: "wheat",
                   cursor: "pointer",
                   zIndex: 10,
+                  backgroundColor: "rgb(50,10,20)",
                 }
               : {
                   transition: "all 0.1s ease",
